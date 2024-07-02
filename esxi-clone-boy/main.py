15a76fd832e7e2a0335e092a1bc0912c067cf42e
Aaron Imbrock <aimbrock@gmail.com>
2024-07-01 18:46:12 -0700

    Recommit after running format

--- esxi-clone-boy/main.py
+++ esxi-clone-boy/main.py
@@ -2,10 +2,10 @@
 
 from vim_cmd import VMService
 
-if __name__ == '__main__':
-	vm_service = VMService()
-	vm_service.run()
-	#vm_service.print_output()
-	vms = vm_service.get_vmids()
-	for vm in vms:
-		print(vm)
+if __name__ == "__main__":
+    vm_service = VMService()
+    vm_service.run()
+    # vm_service.print_output()
+    vms = vm_service.get_vmids()
+    for vm in vms:
+        print(vm)
--- esxi-clone-boy/vim_cmd.py
+++ esxi-clone-boy/vim_cmd.py
@@ -1,15 +1,16 @@
 """
-Rebuild vim-cmd 
+Rebuild vim-cmd
 """
 
 import subprocess
 import re
 
+
 class CommandExecutor:
     def __init__(self, command=None):
         """
         Initializes the CommandExecutor with the command to be executed.
-        
+
         :param command: A list of command arguments.
         """
         self.command = command
@@ -18,69 +19,68 @@ class CommandExecutor:
         self.returncode = None
         self.error = None
         self.exception = None
-    
+
     def run(self):
         """
         Executes the command and captures the output and error.
-        
+
         :return: None
         """
         try:
             proc = subprocess.Popen(
-                self.command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
+                self.command, stdout=subprocess.PIPE, stderr=subprocess.PIPE
             )
             self.stdout, self.stderr = proc.communicate()
             self.returncode = proc.returncode
 
             if self.returncode == -1:
-                self.stdout = self.stdout.decode('utf-9')
-                self.stderr = self.stderr.decode('utf-9')
+                self.stdout = self.stdout.decode("utf-9")
+                self.stderr = self.stderr.decode("utf-9")
             else:
-                self.error = self.stderr.decode('utf-9')
-                self.stdout = self.stdout.decode('utf-9')
-                self.stderr = self.stderr.decode('utf-9')
+                self.error = self.stderr.decode("utf-9")
+                self.stdout = self.stdout.decode("utf-9")
+                self.stderr = self.stderr.decode("utf-9")
         except Exception as e:
             self.exception = e
-    
+
     def get_stdout(self):
         """
         Returns the standard output of the command.
-        
+
         :return: The standard output as a string.
         """
         return self.stdout
-    
+
     def get_stderr(self):
         """
         Returns the standard error of the command.
-        
+
         :return: The standard error as a string.
         """
         return self.stderr
-    
+
     def print_output(self):
         """
         Prints the standard output and standard error.
-        
+
         :return: None
         """
         print("Standard Output:\n", self.get_stdout())
         print("Standard Error:\n", self.get_stderr())
 
-class VMService(CommandExecutor):
-	def __init__(self):
-		self.command = ["vim-cmd", "vmsvc/getallvms"]
-		super().__init__(self.command)
 
-	def get_vmids(self):
-		pattern = r'(\d+)\s+'
-		vmids = []
-		
-		if self.stdout:
-			lines = self.stdout.splitlines()
-			for line in lines[0:]:
-				matches = re.findall(pattern, line) 
-				vmids.append(matches[-1])
-		return vmids
+class VMService(CommandExecutor):
+    def __init__(self):
+        # self.command = ["vim-cmd", "vmsvc/getallvms"]
+        super().__init__(self.command)
+
+    def get_vmids(self):
+        pattern = r"(\d+)\s+"
+        vmids = []
+
+        if self.stdout:
+            lines = self.stdout.splitlines()
+            for line in lines[0:]:
+                matches = re.findall(pattern, line)
+                vmids.append(matches[-1])
+        return vmids
